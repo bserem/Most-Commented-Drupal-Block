d4cd556a83fc2bf65beabbac0ea5cb418c2d4ccf
Bill Seremetis <bserem@gmail.com>
2011-01-18 11:45:46 +0200

    Initial Release (v0.1)
    with the style and the views-exported file you can create
    a bargraph similar to the new Engadgets design
    
    just change the css to get the colors you like!
    
    (this is also an attempt for me to start working with git
    and version controll)

---
+++ most_commented_view.php
@@ -0,0 +1,118 @@
+$view = new view;
+$view->name = 'most_commented';
+$view->description = 'most commented nodes';
+$view->tag = 'custom';
+$view->view_php = '';
+$view->base_table = 'node';
+$view->is_cacheable = FALSE;
+$view->api_version = 2;
+$view->disabled = FALSE; /* Edit this to true to make a default view disabled initially */
+$handler = $view->new_display('default', 'Defaults', 'default');
+$handler->override_option('fields', array(
+  'comment_count' => array(
+    'label' => '',
+    'alter' => array(
+      'alter_text' => 0,
+      'text' => '',
+      'make_link' => 0,
+      'path' => '',
+      'link_class' => '',
+      'alt' => '',
+      'prefix' => '',
+      'suffix' => '',
+      'target' => '',
+      'help' => '',
+      'trim' => 0,
+      'max_length' => '',
+      'word_boundary' => 1,
+      'ellipsis' => 1,
+      'html' => 0,
+      'strip_tags' => 0,
+    ),
+    'empty' => '',
+    'hide_empty' => 0,
+    'empty_zero' => 0,
+    'set_precision' => FALSE,
+    'precision' => 0,
+    'decimal' => '.',
+    'separator' => ',',
+    'prefix' => '',
+    'suffix' => '',
+    'exclude' => 1,
+    'id' => 'comment_count',
+    'table' => 'node_comment_statistics',
+    'field' => 'comment_count',
+    'relationship' => 'none',
+  ),
+  'title' => array(
+    'label' => '',
+    'alter' => array(
+      'alter_text' => 1,
+      'text' => '<div class="most-commented"><span class="most-commented-caption">[title]</span><span class="most-commented-commentsnumber">[comment_count]</span></div>',
+      'make_link' => 0,
+      'path' => '',
+      'link_class' => '',
+      'alt' => '',
+      'prefix' => '',
+      'suffix' => '',
+      'target' => '',
+      'help' => '',
+      'trim' => 0,
+      'max_length' => '',
+      'word_boundary' => 1,
+      'ellipsis' => 1,
+      'html' => 0,
+      'strip_tags' => 0,
+    ),
+    'empty' => '',
+    'hide_empty' => 0,
+    'empty_zero' => 0,
+    'link_to_node' => 1,
+    'exclude' => 0,
+    'id' => 'title',
+    'table' => 'node',
+    'field' => 'title',
+    'relationship' => 'none',
+  ),
+));
+$handler->override_option('sorts', array(
+  'comment_count' => array(
+    'order' => 'DESC',
+    'id' => 'comment_count',
+    'table' => 'node_comment_statistics',
+    'field' => 'comment_count',
+    'relationship' => 'none',
+  ),
+));
+$handler->override_option('filters', array(
+  'type' => array(
+    'operator' => 'in',
+    'value' => array(
+      'story' => 'story',
+    ),
+    'group' => '0',
+    'exposed' => FALSE,
+    'expose' => array(
+      'operator' => FALSE,
+      'label' => '',
+    ),
+    'id' => 'type',
+    'table' => 'node',
+    'field' => 'type',
+    'relationship' => 'none',
+  ),
+));
+$handler->override_option('access', array(
+  'type' => 'none',
+));
+$handler->override_option('cache', array(
+  'type' => 'none',
+));
+$handler->override_option('title', 'Most Commented');
+$handler->override_option('items_per_page', 5);
+$handler->override_option('style_options', array(
+  'grouping' => '',
+));
+$handler = $view->new_display('block', 'Block', 'block_1');
+$handler->override_option('block_description', 'Most Commented News');
+$handler->override_option('block_caching', -1);
